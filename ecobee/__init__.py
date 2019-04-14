d7cd54c6e6bd0a0f7563d08f705a69478bb07c98
Harry <harryliu1995@gmail.com>
2019-04-14 13:30:52 -0400

    change db name

--- ecobee/__init__.py
+++ ecobee/__init__.py
@@ -7,7 +7,7 @@ def create_app():
 	
 	app = Flask(__name__)
 
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1/mydb'
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1/ecobee_db'
 
 	db.init_app(app)
 
