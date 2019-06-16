29f88c0393bfe3bca8d2114c6a7812717e84c1bf
Harry Liu <harryliu1995@gmail.com>
2019-06-15 22:04:19 -0400

    sqlite db

--- ecobee/config.py
+++ ecobee/config.py
@@ -8,8 +8,8 @@ parent_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 
 class Config:
     SECRET_KEY = "secret"
-    # SQLALCHEMY_DATABASE_URI = f"sqlite:///{parent_path}/site.db"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{parent_path}/site.db"
     # SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost/ecobee'
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/ecobee"
+    # SQLALCHEMY_DATABASE_URI = "postgresql://localhost/ecobee"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
