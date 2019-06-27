28db28a4deebce44584fca2647591c6f0d7477b6
Harry Liu <harryliu1995@gmail.com>
2019-06-27 08:36:57 -0400

    refactor

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
+import { AppsComponent } from './components/apps/apps.component'
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RegisterAppComponent } from './components/register-app/register-app.component';
+import { AppFormComponent } from './components/register-app/components/app-form/app-form.component';
+
+const routes: Routes = [
+
+    {path: '', component: MainComponent},
+
+    // User
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+
+    // App
+    {path: 'register_app', component: RegisterAppComponent},
+    {path: 'apps', component: AppsComponent},
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
+export const RoutingComponents = [MainComponent,
+                                NavbarComponent,
+                                AppsComponent,
+                                LoginComponent,
+                                RegisterComponent,
+                                RegisterAppComponent,
+                                AppFormComponent]
---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule, RoutingComponents  } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RoutingComponents,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
