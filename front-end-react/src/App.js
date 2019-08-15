73963515913c7a88a7a0f0d2b5e5471711609597
Harry Liu <harryliu1995@gmail.com>
2019-08-15 12:28:05 -0400

    add routing to app

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Main from './components/Main.js'
-import Navbar from './components/Navbar.js'
+import Main from './components/Main.js';
+import Navbar from './components/Navbar.js';
+import Apps from './components/Apps.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <main class='container pt-3 pb-3 text-light'>
-        <Main />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main className='container pt-3 pb-3 text-light'>
+          <Switch>
+            <Route path='/' exact component={Main} />
+            <Route path='/apps' component={Apps} />
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
