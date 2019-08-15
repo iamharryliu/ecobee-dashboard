62494a966fe10491c5b72ba56b7ab460eac44dac
Harry Liu <harryliu1995@gmail.com>
2019-08-15 12:31:04 -0400

    route links and map class -> className

--- front-end-react/src/components/Main.js
+++ front-end-react/src/components/Main.js
@@ -2,18 +2,19 @@ import React from 'react'
 
 const Main = () => {
     return (
-        <div class='jumbotron mt-3 mb-3'>
-            <div class='text-dark'>
-                <h1 class='display-4'>Ecobee Dash</h1>
-                <p class='lead'>Have control over your thermostat.</p>
-                <hr class='my-4' />
+        <div className='jumbotron mt-3 mb-3'>
+            <div className='text-dark'>
+                <h1 className='display-4'>Ecobee Dash</h1>
+                <p className='lead'>Have control over your thermostat.</p>
+                <hr className='my-4' />
                 <p>Minimalist Ecobee web app designed to have full control over your Ecobee app.</p>
             </div>
-            <p class='lead text-light'>
-                <a class='btn btn-success btn-lg'>View Apps</a>
-                <a class='btn btn-success btn-lg'>Register App</a>
+            <p className='lead text-light'>
+                <a href='/apps' className='btn btn-success btn-lg'>View Apps</a>
+                <a href='/apps/register' className='btn btn-success btn-lg'>Register App</a>
             </p>
         </div>
     )
 }
 export default Main
+
--- front-end-react/src/components/Navbar.js
+++ front-end-react/src/components/Navbar.js
@@ -2,36 +2,36 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top">
-            <div class='container'>
-                <a class="navbar-brand">
-                    <img src='favicon.ico' width="50" height="50" class="d-inline-block align-top" alt="" />
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top">
+            <div className='container'>
+                <a href='/' className="navbar-brand">
+                    <img src='favicon.ico' width="50" height="50" className="d-inline-block align-top" alt="" />
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link">Home</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a href='/' className="nav-link">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link">Your Apps</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link">Register</a>
+                        <li className="nav-item">
+                            <a href='/apps' className="nav-link">Your Apps</a>
                         </li>
                     </ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link">Logout</a>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a href='/login' className="nav-link">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href='/register' className="nav-link">Register</a>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a href='logout' className="nav-link">Logout</a>
                         </li>
                     </ul >
                 </div >
