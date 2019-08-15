22eb1c31d3254b99f51420e6b13a169b08feba71
Harry Liu <harryliu1995@gmail.com>
2019-08-15 15:31:06 -0400

    add components to app

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 import './App.css';
 import Main from './components/Main.js';
 import Navbar from './components/Navbar.js';
+import Register from './components/Register'
+import Login from './components/Login.js'
 import Apps from './components/Apps.js';
+import Thermostats from './components/Thermostats.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -14,7 +17,10 @@ function App() {
         <main className='container pt-3 pb-3 text-light'>
           <Switch>
             <Route path='/' exact component={Main} />
-            <Route path='/apps' component={Apps} />
+            <Route path='/register' component={Register} />
+            <Route path='/login' component={Login} />
+            <Route path='/apps' exact component={Apps} />
+            <Route path='/thermostats' component={Thermostats} />
           </Switch>
         </main>
       </div>
