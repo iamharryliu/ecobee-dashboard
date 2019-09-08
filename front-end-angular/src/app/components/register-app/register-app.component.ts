5f766156cf424055079c2dfbdce4cdd49957d52e
Harry Liu <harryliu1995@gmail.com>
2019-09-08 16:52:40 -0400

    update to use reactive forms

--- front-end-angular/src/app/components/register-app/register-app.component.ts
+++ front-end-angular/src/app/components/register-app/register-app.component.ts
@@ -1,14 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms'
 import { AppService } from '../../app.service'
 import { Router } from '@angular/router';
 
-class appForm {
-  constructor(
-    public appName: string,
-    public apiKey: string,
-    public authorizationCode: string
-  ) { }
-}
+declare var $: any;
 
 @Component({
   selector: 'app-register-app',
@@ -17,45 +12,63 @@ class appForm {
 })
 export class RegisterAppComponent implements OnInit {
 
-  constructor(private _AppService: AppService,
-    private _router: Router) { }
+  constructor(
+    private _FormBuilder: FormBuilder,
+    private _AppService: AppService,
+    private _router: Router
+  ) { }
 
-  form = new appForm('', '', '')
+  public nameMinLength = 4;
+  public keyLength = 32;
+  public authorizationCodeLength = 32;
+
+  public authorizeForm = this._FormBuilder.group({
+    key: ['', [Validators.required, Validators.minLength(this.keyLength), Validators.maxLength(this.keyLength)]]
+  })
+  public pin: string;
+  public registerForm = this._FormBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(this.nameMinLength)]],
+    key: [''],
+    authorizationCode: [''],
+  })
 
   ngOnInit() { }
 
   authorizeApp() {
-    this._AppService.authorizeApp(this.form.apiKey)
-      .subscribe(r => {
-        this.handleAuthorizeAppResponse(r)
+    this._AppService.authorizeApp(this.authorizeForm.value.key)
+      .subscribe(response => {
+        if (response.success) {
+          console.log('Successfully authorized app.')
+          this.pin = response.data.pin
+          this.registerForm.patchValue({ key: this.authorizeForm.value.key, authorizationCode: response.data.authorization_code })
+          $('#exampleModal').modal('toggle')
+        }
+        else {
+          console.log('Could not successfully authorize app.')
+        }
       });
   }
 
-  handleAuthorizeAppResponse(r: any) {
-    if (r.success) {
-      console.log('Successfully authorized app.')
-      alert(r.data.pin)
-      this.form.authorizationCode = r.data.authorization_code
-    }
-    else {
-      console.log('Could not successfully authorize app.')
-    }
-  }
-
-  onApiKeyChange() {
-    this.form.authorizationCode = ''
-  }
-
-  onSubmit() {
-    this._AppService.createApp(this.form)
-      .subscribe(r => {
-        if (r.success) {
+  registerApp() {
+    this._AppService.createApp(this.registerForm.value)
+      .subscribe(response => {
+        if (response.success) {
+          console.log('Successfully registered app.')
+          $('#exampleModal').modal('toggle')
           this._router.navigate(['/apps'])
         }
         else {
           alert('Could not register app.')
         }
       });
+  }
+
+  get key() {
+    return this.authorizeForm.get('key')
+  }
+
+  get name() {
+    return this.registerForm.get('name')
   }
 
 
