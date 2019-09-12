276f3edc4325921bf84c182737037c701e0dff1b
Harry Liu <harryliu1995@gmail.com>
2019-09-12 07:39:28 -0400

    wait for data to load

--- front-end-react/src/components/Thermostats.js
+++ front-end-react/src/components/Thermostats.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import axios from 'axios';
 
 
@@ -7,6 +7,7 @@ class Thermostats extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            dataLoaded: false,
             thermostats: [],
             errMessage: ''
         }
@@ -15,7 +16,10 @@ class Thermostats extends Component {
     componentDidMount() {
         axios.get('http://localhost:5000/getUserThermostats', { withCredentials: true })
             .then(response => {
-                this.setState({ thermostats: response.data })
+                this.setState({
+                    thermostats: response.data,
+                    dataLoaded: true
+                })
             })
             .catch(error => {
                 console.log(error)
@@ -24,36 +28,39 @@ class Thermostats extends Component {
     }
 
     render() {
-        const { thermostats, errMessage } = this.state
+        const { dataLoaded, thermostats, errMessage } = this.state
         return (
             <div>
-                <div className='row'>
-                    <h1 className='col-12'> Thermostats</h1 >
-                    {
-                        errMessage ? <div className='col-12'>{errMessage}</div> : null
-                    }
-                    {
-                        thermostats.length ?
-                            thermostats.map(thermostat =>
-                                <div className='col-12 mt-3 mb-3 app'>
-                                    <div className='card bg-dark'>
-                                        <div className='card-body'>
-                                            <h2 className='d-inline'>{thermostat.data.name}</h2>
-                                            <div className='float-right'>
-                                                <Link to='/' className='btn btn-info'>View</Link>
+                {dataLoaded &&
+                    <div className='row'>
+                        <h1 className='col-12'> Thermostats</h1 >
+                        {
+                            errMessage ? <div className='col-12'>{errMessage}</div> : null
+                        }
+                        {
+                            thermostats.length ?
+                                thermostats.map(thermostat =>
+                                    <div key={thermostat.api_key} className='col-12 mt-3 mb-3 app'>
+                                        <div className='card bg-dark'>
+                                            <div className='card-body'>
+                                                <h2 className='d-inline'>{thermostat.data.name}</h2>
+                                                <div className='float-right'>
+                                                    <Link to='/' className='btn btn-info'>View</Link>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            ) :
-                            <div className='col-12 mt-3 mb-3'>
-                                You currently have no thermostats registered with this account.
+                                ) :
+                                <div className='col-12 mt-3 mb-3'>
+                                    You currently have no thermostats registered with this account.
                         </div>
-                    }
-                </div>
+                        }
+                    </div>
+                }
             </div>
+
         );
     }
 }
 
-export default Thermostats;
+export default withRouter(Thermostats);
