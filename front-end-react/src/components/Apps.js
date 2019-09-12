f695cc0defe6f5135f5c8057c9734dc2081776d6
Harry Liu <harryliu1995@gmail.com>
2019-09-12 07:40:27 -0400

    wait for onload

--- front-end-react/src/components/Apps.js
+++ front-end-react/src/components/Apps.js
@@ -6,6 +6,7 @@ class Apps extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            dataLoaded: false,
             apps: [],
             errMessage: ''
         }
@@ -14,7 +15,10 @@ class Apps extends Component {
     componentDidMount() {
         axios.get('http://localhost:5000/apps', { withCredentials: true })
             .then(response => {
-                this.setState({ apps: response.data })
+                this.setState({
+                    dataLoaded: true,
+                    apps: response.data
+                })
             })
             .catch(error => {
                 console.log(error)
@@ -23,40 +27,41 @@ class Apps extends Component {
     }
 
     render() {
-        const { apps, errMessage } = this.state
+        const { dataLoaded, apps, errMessage } = this.state
         return (
             <div>
-                <div className='row'>
-                    <h1 className='col-6'>Apps</h1>
-                    <div className='col-6'>
-                        <Link to='blank' className='btn btn-primary btn-lg float-right'>Register App</Link>
-                    </div>
-                    {
-                        errMessage ? <div className='col-12'>{errMessage}</div> : null
-                    }
-                    {
-                        apps.length ?
-                            apps.map(app =>
-                                <div key={app.key} className='col-12 mt-3 mb-3 app'>
-                                    <div className='card bg-dark'>
-                                        <div className='card-body'>
-                                            <h2 className='d-inline'>{app.name}</h2>
-                                            <div className='float-right'>
-                                                <Link to='thermostats' className='btn btn-info'>View</Link>
-                                                <Link to='blank' className='btn btn-info'>Reauthorize</Link>
-                                                <Link to='blank' className='btn btn-info'>Update App Credentials</Link>
-                                                <button className='btn btn-danger'>Delete</button>
+                {dataLoaded &&
+                    <div className='row'>
+                        <h1 className='col-6'>Apps</h1>
+                        <div className='col-6'>
+                            <Link to='blank' className='btn btn-primary btn-lg float-right'>Register App</Link>
+                        </div>
+                        {
+                            errMessage ? <div className='col-12'>{errMessage}</div> : null
+                        }
+                        {
+                            apps.length ?
+                                apps.map(app =>
+                                    <div key={app.key} className='col-12 mt-3 mb-3 app'>
+                                        <div className='card bg-dark'>
+                                            <div className='card-body'>
+                                                <h2 className='d-inline'>{app.name}</h2>
+                                                <div className='float-right'>
+                                                    <Link to='thermostats' className='btn btn-info'>View</Link>
+                                                    <Link to='blank' className='btn btn-info'>Reauthorize</Link>
+                                                    <Link to='blank' className='btn btn-info'>Update App Credentials</Link>
+                                                    <button className='btn btn-danger'>Delete</button>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div >
-                            ) :
-                            <div className='col-12 mt-3 mb-3'>
-                                You currently have no apps registered with this account.
+                                    </div >
+                                ) :
+                                <div className='col-12 mt-3 mb-3'>
+                                    You currently have no apps registered with this account.
                         </div>
-
-                    }
-                </div>
+                        }
+                    </div>
+                }
 
             </div >
 
