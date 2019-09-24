86603c91b3a1957d5bee22c31292050ef59d43ba
Harry Liu <harryliu1995@gmail.com>
2019-09-24 11:13:33 -0400

    add cors, whilelist, and allow credentials

--- back-end-django/django_app/settings.py
+++ back-end-django/django_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -113,3 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:4200'
+]
+
+CORS_ALLOW_CREDENTIALS = True
