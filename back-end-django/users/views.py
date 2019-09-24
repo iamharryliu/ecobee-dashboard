4edc706630c0f8050424ef0a841c2c277e778e1b
Harry Liu <harryliu1995@gmail.com>
2019-09-24 11:13:06 -0400

    finish views

--- back-end-django/users/views.py
+++ back-end-django/users/views.py
@@ -1,32 +1,33 @@
-import json
-from django.shortcuts import redirect
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from .utils import register_user, get_login_status, login_user, logout_user
 
 # Create your views here.
 @csrf_exempt
-def register(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        return JsonResponse({"test": "success"})
+def _register_user(request):
+    try:
+        register_user(request)
+    except:
+        success = False
+    else:
+        success = True
+    return JsonResponse({"success": success})
 
 
 @csrf_exempt
-def get_logged_in_status(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        return JsonResponse({"test": "success"})
+def _get_login_status(request):
+    status = get_login_status(request)
+    return JsonResponse({"success": True, "status": status})
 
 
 @csrf_exempt
-def login_user(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        return JsonResponse({"test": "success"})
+def _login_user(request):
+    success = True if login_user(request) else False
+    return JsonResponse({"success": success})
 
 
 @csrf_exempt
-def logout_user(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        return JsonResponse({"test": "success"})
+def _logout_user(request):
+    logout_user(request)
+    return JsonResponse({"success": True})
+
