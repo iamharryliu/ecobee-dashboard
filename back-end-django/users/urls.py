0e4988f2331bdc9332e90969bee27de657545127
Harry Liu <harryliu1995@gmail.com>
2019-09-24 11:12:06 -0400

    update url routes

--- back-end-django/users/urls.py
+++ back-end-django/users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register", views.register),
-    path("getLoggedInStatus", views.register),
-    path("login", views.register),
-    path("logout", views.register),
+    path("register", views._register_user),
+    path("loginStatus", views._get_login_status),
+    path("login", views._login_user),
+    path("logout", views._logout_user),
 ]
 
