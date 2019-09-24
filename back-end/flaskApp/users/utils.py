3ff57e3e56297a8d98f13d7f50b0e711fd042738
Harry Liu <harryliu1995@gmail.com>
2019-09-23 21:14:59 -0400

    refactor out user route utility

---
+++ back-end/flaskApp/users/utils.py
@@ -0,0 +1,37 @@
+from flask import request, session
+from flask_cors import cross_origin
+from flask_login import login_user, current_user, logout_user, login_required
+from flaskApp import db, bcrypt
+from flaskApp.models import User
+import json
+
+
+def register():
+    data = json.loads(request.data)
+    username = data["username"]
+    email = data["email"]
+    password = data["password"]
+    hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+    user = User(username=username, email=email, password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+
+
+def get_login_status():
+    return current_user.is_authenticated
+
+
+def login():
+    data = json.loads(request.data)
+    email = data["email"]
+    password = data["password"]
+    remember = data["remember"]
+    user = User.query.filter_by(email=email).first()
+    if user and bcrypt.check_password_hash(user.password, password):
+        login_user(user, remember=remember)
+        return True
+    else:
+        return False
+
+def logout():
+    logout_user()
