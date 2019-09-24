aab738387e7a50c894f2fc0574bc87dc8b492890
Harry Liu <harryliu1995@gmail.com>
2019-09-24 11:10:18 -0400

    allow db migrate column changes

--- back-end/manage.py
+++ back-end/manage.py
@@ -4,12 +4,10 @@ from flask_migrate import Migrate, MigrateCommand
 from flaskApp import create_app, db
 
 app = create_app()
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, compare_type=True)
 manager = Manager(app)
-
 manager.add_command("runserver", Server())
 manager.add_command("db", MigrateCommand)
-
 from flaskApp.models import *
 
 if __name__ == "__main__":
