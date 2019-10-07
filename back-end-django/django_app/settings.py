4206de7d51a4a2aa55fd26de9f23522de7857556
Harry Liu <harryliu1995@gmail.com>
2019-10-07 11:32:59 -0400

    add apps config

--- back-end-django/django_app/settings.py
+++ back-end-django/django_app/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'corsheaders',
     "users.apps.UsersConfig",
+    "apps.apps.AppsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
