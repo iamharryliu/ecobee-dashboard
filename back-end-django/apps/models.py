a16087f7e357975508a6ab01eb736ce6b2bf945f
Harry Liu <harryliu1995@gmail.com>
2019-10-07 11:33:20 -0400

    create app model

--- back-end-django/apps/models.py
+++ back-end-django/apps/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class App(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    api_key = models.CharField(max_length=32)
+    authorization_code = models.CharField(max_length=32)
+    access_token = models.CharField(max_length=32)
+    refresh_token = models.CharField(max_length=32)
