927810e5dcd055ab72a28bb8dc50d4419d7afbfa
Harry Liu <harryliu1995@gmail.com>
2019-10-07 11:33:45 -0400

    add views

--- back-end-django/apps/views.py
+++ back-end-django/apps/views.py
@@ -1,34 +1,43 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from .utils import register_user, get_login_status, login_user, logout_user
+from .utils import authorize
 
 # Create your views here.
 @csrf_exempt
-def _authorize(request):
-    success = True
-    return JsonResponse({"success": success})
+def _authorize(request, key):
+    try:
+        pin, authorization_code = authorize(key)
+    except:
+        print("Unsuccessfuly authorized app.")
+        success = False
+        data = None
+    else:
+        print("Successfully authorized app.")
+        success = True
+        data = {"pin": pin, "authorization_code": authorization_code}
+    return JsonResponse({"success": success, "data": data})
 
 
-@csrf_exempt
-def _create(request):
-    success = True
-    return JsonResponse({"success": success})
+# @csrf_exempt
+# def _create(request):
+#     success = True
+#     return JsonResponse({"success": success})
 
 
-@csrf_exempt
-def _update(request):
-    success = True
-    return JsonResponse({"success": success})
+# @csrf_exempt
+# def _update(request):
+#     success = True
+#     return JsonResponse({"success": success})
 
 
-@csrf_exempt
-def _delete(request):
-    success = True
-    return JsonResponse({"success": success})
+# @csrf_exempt
+# def _delete(request):
+#     success = True
+#     return JsonResponse({"success": success})
 
 
-@csrf_exempt
-def _apps(request):
-    success = True
-    return JsonResponse({"success": success})
+# @csrf_exempt
+# def _apps(request):
+#     success = True
+#     return JsonResponse({"success": success})
 
