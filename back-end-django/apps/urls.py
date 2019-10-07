23d9ccbcbfa2dddb347795b517aeb2a64c573298
Harry Liu <harryliu1995@gmail.com>
2019-10-07 11:33:40 -0400

    add urls

--- back-end-django/apps/urls.py
+++ back-end-django/apps/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("authorize", views._authorize),
-    path("create", views._create),
-    path("update", views._update),
-    path("delete", views._delete),
-    path("get", views._apps),
+    path("authorize/<str:key>", views._authorize),
+    # path("create", views._create),
+    # path("update", views._update),
+    # path("delete", views._delete),
+    # path("get", views._apps),
 ]
-
