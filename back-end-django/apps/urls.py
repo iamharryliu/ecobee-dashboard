171daaf6cbf936ea6ea6d2b665b97b82f1d0fa07
Harry Liu <harryliu1995@gmail.com>
2019-10-09 09:56:15 -0400

    add thermostat urls

--- back-end-django/apps/urls.py
+++ back-end-django/apps/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path("update", views._update),
     path("delete/<str:key>", views._delete),
     path("", views._get_apps),
+    path("getUserThermostats", views._get_user_thermostats),
+    path("getAppThermostats/<str:key>", views._get_app_thermostats),
+    path("thermostat/<str:identifier>", views._get_thermostat),
+    path(
+        "thermostats/<str:key>/<str:identifier>/runtimeReport",
+        views._get_runtime_report,
+    ),
 ]
