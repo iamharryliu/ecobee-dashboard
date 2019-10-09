ae6cd91377ba36e455b42fde9d553f2353840735
Harry Liu <harryliu1995@gmail.com>
2019-10-09 09:56:41 -0400

    add thermostat views

--- back-end-django/apps/views.py
+++ back-end-django/apps/views.py
@@ -1,6 +1,17 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from .utils import check_api, authorize, create_app, update_app, delete_app, get_apps
+from .utils import (
+    check_api,
+    authorize,
+    create_app,
+    update_app,
+    delete_app,
+    get_apps,
+    get_user_thermostats,
+    get_app_thermostats,
+    get_thermostat,
+    get_runtime_report,
+)
 
 
 @csrf_exempt
@@ -72,3 +83,63 @@ def _get_apps(request):
     apps = get_apps(request)
     return JsonResponse(apps, safe=False)
 
+
+# Thermostats
+
+
+@csrf_exempt
+def _get_user_thermostats(request):
+    thermostats = get_user_thermostats(request)
+    return JsonResponse(thermostats, safe=False)
+
+
+@csrf_exempt
+def _get_app_thermostats(request, key):
+    thermostats = get_app_thermostats(key)
+    return JsonResponse(thermostats, safe=False)
+
+
+@csrf_exempt
+def _get_thermostat(request, identifier):
+    thermostat = get_thermostat(request, identifier)
+    success = True if thermostat else False
+    return JsonResponse({"success": success, "thermostat": thermostat})
+
+
+@csrf_exempt
+def _get_runtime_report(request, key, identifier):
+    runtimeReport = get_runtime_report(key, identifier)
+    return JsonResponse(runtimeReport)
+
+
+# # Thermostat Actions
+
+
+# @apps_blueprint.route("/setHvacMode", methods=["POST"])
+# @cross_origin(supports_credentials=True)
+# def _set_hvac_mode():
+#     return jsonify({"success": set_hvac_mode()})
+
+
+# @apps_blueprint.route("/resume", methods=["POST"])
+# @cross_origin(supports_credentials=True)
+# def _resume():
+#     return jsonify({"success": resume()})
+
+
+# @apps_blueprint.route("/setClimate", methods=["POST"])
+# @cross_origin(supports_credentials=True)
+# def _set_climate():
+#     return jsonify({"success": set_climate()})
+
+
+# @apps_blueprint.route("/setTemperature", methods=["POST"])
+# @cross_origin(supports_credentials=True)
+# def _set_temperature_hold():
+#     return jsonify({"success": set_temperature_hold()})
+
+
+# @apps_blueprint.route("/sendMessage", methods=["POST"])
+# @cross_origin(supports_credentials=True)
+# def _send_message():
+#     return jsonify({"success": send_message()})
