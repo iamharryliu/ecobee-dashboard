d39cde26229003690be095b5af378df24075fcce
Harry Liu <harryliu1995@gmail.com>
2019-10-13 18:42:27 -0400

    import bootstrap packages

--- front-end-react/src/index.js
+++ front-end-react/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import './index.css';
 
 const rootElement = document.getElementById("root")
 ReactDOM.render(
