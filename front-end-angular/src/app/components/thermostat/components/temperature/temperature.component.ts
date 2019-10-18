ddaa73e130e6c67dda742fdcdab2d649f8ede13d
Harry Liu <harryliu1995@gmail.com>
2019-10-17 20:01:27 -0400

    refactor

--- front-end-angular/src/app/components/thermostat/components/temperature/temperature.component.ts
+++ front-end-angular/src/app/components/thermostat/components/temperature/temperature.component.ts
@@ -9,32 +9,25 @@ import { AppService } from '../../../../app.service'
 export class TemperatureComponent implements OnInit {
 
   @Input() public thermostat: any;
-  @Output() public setTemperature: EventEmitter<string> = new EventEmitter();
+  @Input() public currentClimateRefTemp: number;
+  @Output() public updateThermostat: EventEmitter<string> = new EventEmitter();
 
   public temperatureOptions = [18.0, 18.5, 19.0, 19.5, 20.0, 20.5, 21.0, 21.5, 22.0, 22.5, 23.0, 23.5, 24.0];
 
-  constructor(
-    private _AppService: AppService,
-  ) { }
+  constructor(private _AppService: AppService) { }
 
   ngOnInit() { }
 
-  // Actions
+  get actualTemperature() { return this._AppService.ecobeeTempToDegrees(this.thermostat.data.runtime.actualTemperature) }
+  get climateRef() { return this._AppService.getCurrentClimateRef(this.thermostat) }
 
-  onChange(temperature: string) {
-    this.setTemperature.emit(temperature);
-  }
+  // Thermostat actions.
 
-  get actualTemperature() {
-    return this._AppService.ecobeeTempToDegrees(this.thermostat.data.runtime.actualTemperature)
-  }
-
-  get climateRef() {
-    return this._AppService.getThermostatClimateRef(this.thermostat)
-  }
-
-  get climateRefTemp() {
-    return this._AppService.getThermostatClimateRefTemp(this.thermostat)
+  setTemperature(temperature: string) {
+    this._AppService.setTemperature(this.thermostat, parseFloat(temperature)).subscribe(_ => {
+      console.log('Successfully set temperature.');
+      this.updateThermostat.emit()
+    });
   }
 
 }
