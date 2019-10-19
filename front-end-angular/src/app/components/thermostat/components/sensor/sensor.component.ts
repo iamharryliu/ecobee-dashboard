63e9c50a6e5514d982ffbd96a976398c7218b628
Harry Liu <harryliu1995@gmail.com>
2019-10-19 12:07:51 -0400

    refactor sensor component

--- front-end-angular/src/app/components/thermostat/components/sensor/sensor.component.ts
+++ front-end-angular/src/app/components/thermostat/components/sensor/sensor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { RemoteSensor } from 'app'
+
 @Component({
   selector: 'app-sensor',
   templateUrl: './sensor.component.html',
@@ -7,11 +9,50 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SensorComponent implements OnInit {
 
-  @Input() public sensor;
+  @Input() public sensor: RemoteSensor;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  getTemperature(sensor: RemoteSensor) {
+    let capabilities = sensor.capability
+    for (let i = 0; i < capabilities.length; i++) {
+      let capability = capabilities[i]
+      if (capability.type === 'temperature') {
+        return this.ecobeeTempToDegrees(Number(capability.value))
+      }
+    }
+    return false
+  }
+
+  getOccupancy(sensor: RemoteSensor) {
+    let capabilities = sensor.capability
+    for (let i = 0; i < capabilities.length; i++) {
+      let capability = capabilities[i]
+      if (capability.type === 'occupancy') {
+        return capability.value == 'true'
+      }
+    }
+  }
+
+  getHumidity(sensor: RemoteSensor) {
+    let capabilities = sensor.capability
+    for (let i = 0; i < capabilities.length; i++) {
+      let capability = capabilities[i]
+      if (capability.type === 'humidity') {
+        return capability.value
+      }
+    }
+    return false
+  }
+
+  ecobeeTempToDegrees(temperature: number) {
+    temperature = (temperature / 10 - 32) * 5 / 9
+    temperature = Math.round(temperature * 10) / 10
+    return temperature
+  }
+
 
 }
