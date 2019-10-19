6f6884c5aa2ede9b6f999471b8379a676f6670af
Harry Liu <harryliu1995@gmail.com>
2019-10-19 12:07:37 -0400

    remove reserialization of sensors

--- front-end-angular/src/app/components/thermostat/thermostat.component.ts
+++ front-end-angular/src/app/components/thermostat/thermostat.component.ts
@@ -44,34 +44,10 @@ export class ThermostatComponent implements OnInit {
   setData(thermostat: App) {
     this.thermostat = thermostat;
     this.remoteSensors = thermostat.data.remoteSensors;
-    this.reserializeSensors()
   }
 
   get currentClimateRefTemp() {
     return this._AppService.getCurrentClimateRefTemp(this.thermostat)
-  }
-
-  reserializeSensors() {
-    for (let i = 0; i < this.remoteSensors.length; i++) {
-      let remoteSensor = this.remoteSensors[i]
-      if (remoteSensor.type == 'ecobee3_remote_sensor') {
-        remoteSensor.type = 'remote sensor'
-      }
-      let capabilities = remoteSensor.capability
-      for (let j = 0; j < capabilities.length; j++) {
-        let capability = capabilities[j]
-        if (capability.type == 'temperature') {
-          remoteSensor.temperature = this._AppService.ecobeeTempToDegrees(capability.value)
-        }
-        if (capability.type == 'humidity') {
-          remoteSensor.humidity = capability.value
-        }
-        if (capability.type == 'occupancy') {
-          remoteSensor.occupancy = capability.value == 'true'
-        }
-      }
-    }
-    this.remoteSensors.sort((b: string, a: string) => (a > b ? 1 : -1));
   }
 
 }
