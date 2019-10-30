1d672898915006190fecdbe70a9a8e24e659ab92
Harry Liu <harryliu1995@gmail.com>
2019-10-29 22:12:43 -0400

    remove commented code

--- front-end-angular/src/app/app.service.ts
+++ front-end-angular/src/app/app.service.ts
@@ -114,43 +114,4 @@ export class AppService {
     return this.http.post(url, data)
   }
 
-  // ecobeeTempToDegrees(temperature: number) {
-  //   temperature = (temperature / 10 - 32) * 5 / 9
-  //   temperature = Math.round(temperature * 10) / 10
-  //   return temperature
-  // }
-
-
-  // getCurrentClimateRef(thermostat: App) {
-  //   let currentClimateRef: string;
-  //   if (thermostat.data.events.length) {
-  //     if (thermostat.data.events[0].holdClimateRef == '') {
-  //       currentClimateRef = 'hold'
-  //     }
-  //     else {
-  //       currentClimateRef = thermostat.data.events[0].holdClimateRef
-  //     }
-  //   }
-  //   else {
-  //     currentClimateRef = thermostat.data.program.currentClimateRef
-  //   }
-  //   return currentClimateRef
-  // }
-
-
-  // getCurrentClimateRefTemp(thermostat: App) {
-  //   let temperature: number
-  //   if (thermostat.data.events.length) {
-  //     temperature = thermostat.data.events[0].heatHoldTemp
-  //   }
-  //   else {
-  //     for (let climate of thermostat.data.program.climates) {
-  //       if (this.getCurrentClimateRef(thermostat) == climate.climateRef) {
-  //         temperature = climate.heatTemp
-  //       }
-  //     }
-  //   }
-  //   return this.ecobeeTempToDegrees(temperature)
-  // }
-
 }
